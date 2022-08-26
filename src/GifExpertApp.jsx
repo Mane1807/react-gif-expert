0da596425fa6adb82aa81b4a6c827c1a77b64c8f
mane1807 <hernandezgutierrezjuan7@gmail.com>
2022-08-25 21:29:54 -0500

    primer commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import { AddCategory ,GifGrid } from "./components";
+
+
+export const GifExpertApp = () => {
+
+  const [categories, setCategories] = useState([ 'one Punch']);
+  console.log(categories);
+
+  const onAddCategory=(newCategory)=>{
+    if (categories.includes(newCategory))return;
+
+    console.log(newCategory);
+    //  categories.push(newCategory);
+    setCategories([newCategory,...categories]);
+    // setCategories(cat =>[...cat, newCategory])
+  }
+
+  return (
+    <>
+        <h1>GifExpertApp</h1>
+        <AddCategory 
+        // setCategories={setCategories} 
+        onNewCategory={(value)=> onAddCategory(value)}
+        /> 
+        
+        
+        { categories.map((category)=>(
+            <GifGrid key={category}
+            category={category}
+            />
+
+          ))
+        }
+    </>
+  )
+}
